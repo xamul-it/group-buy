610310f8f6d390dbe6f590d2481031b2e4e187a3
Alessandro Berbenni <aberbenni@gmail.com>
2020-12-29 18:15:26 +0100

    fix #58 and #59

--- grails-app/assets/javascripts/vue/v-register/register.vue.js
+++ grails-app/assets/javascripts/vue/v-register/register.vue.js
@@ -1,22 +1,20 @@
 //vuelidate
-const { required, email, minLength, sameAs, not } = window.validators;
+const { required, email, minLength, maxLength, sameAs, not } = window.validators;
 // import { required, minLength } from 'vuelidate/lib/validators'
 
 //lodash
-const { drop, every, forEach, get, isArray, map, set } = _;
+const { drop, every, forEach, get, isArray, map, set, isUndefined } = _;
 //import { drop, every, forEach, get, isArray, map, set } from 'lodash';
 
 var VRegisterRegister = Vue.component("VRegisterRegister", {
   template: `
         <div class="vue-template">
-            <slot v-bind:$v="$v" v-bind:$props="$props">
-            </slot>
 
             <form action="/register/register" 
                 class="card-body"
                 method="post" 
-                name="loginForm" 
-                id="loginForm"
+                name="registerForm" 
+                id="registerForm"
                 autocomplete="off"
                 >
 
@@ -26,16 +24,18 @@ var VRegisterRegister = Vue.component("VRegisterRegister", {
                     <input :name="inputParams.usernameParameter"
                         :id="inputParams.usernameParameter"
                         type="text"
-                        value="" 
                         required="required"
+                        readonly="readonly"
+                        @focus="onFieldFocus"
                         @blur="$v.username.$touch()"
-                        v-model="username">
+                        v-model="username"
+                        autocomplete="off">
                         <label>{{inputParams.loginUsername}}</label>
                         <!-- alerts -->
-                        <p class="input-alert" v-if="!$v.username.minLength && $v.username.$error">Please insert at least {{ $v.username.$params.minLength.min }} characters.</p>
-                        <p class="input-alert" v-if="!$v.username.required && $v.username.$error">This field must not be empty.</p>
-                        <p class="input-alert" v-if="false && $v.username.$error">The username is taken.</p>
-                        
+                        <p class="input-alert" v-if="!$v.username.minLength && $v.username.$error">Si prega di inserire almeno {{ $v.username.$params.minLength.min }} caratteri.</p>
+                        <p class="input-alert" v-if="!$v.username.required && $v.username.$error">Questo campo non deve essere vuoto.</p>
+                        <p class="input-alert" v-if="errors.usernameErrorUnique">Il nome utente non &egrave disponibile.</p>
+
                 </div>
                 <!-- pre>{{$v.username}}</pre -->
                 
@@ -44,13 +44,16 @@ var VRegisterRegister = Vue.component("VRegisterRegister", {
                         :id="inputParams.emailParameter"
                         type="email" 
                         required="required"
+                        readonly="readonly"
+                        @focus="onFieldFocus"
                         @input="$v.email.$touch()"
                         v-model="email"
-                        >
+                        autocomplete="off">
                         <label>{{inputParams.registerEmail}}</label>
                         <!-- alerts -->
-                        <p class="input-alert" v-if="!$v.email.email && $v.email.$error">Please provide a valid email address.</p>
-                        <p class="input-alert" v-if="!$v.email.required && $v.email.$error">This field must not be empty.</p>
+                        <p class="input-alert" v-if="errors.emailError || !$v.email.email && $v.email.$error">Si prega di fornire un indirizzo e-mail valido.</p>
+                        <p class="input-alert" v-if="!errors.emailError && !$v.email.required && $v.email.$error">Questo campo non deve essere vuoto.</p>
+
                 </div>
                 <!-- pre>{{$v.email}}</pre-->
 
@@ -59,15 +62,22 @@ var VRegisterRegister = Vue.component("VRegisterRegister", {
                         :id="inputParams.passwordParameter"
                         type="password"
                         required="required"
+                        readonly="readonly"
+                        @focus="onFieldFocus"
                         @input="$v.password.$touch()"
-                        v-model="password">
+                        v-model="password"
+                        autocomplete="off">
                         <label>{{inputParams.loginPassword}}</label>
                         <password v-model="password" :strength-meter-only="true"/>
                         <!-- alerts -->
-                        <p class="input-alert" v-if="!$v.password.minLength && $v.password.$error">Please insert at least {{ $v.password.$params.minLength.min }} characters.</p>
-                        <p class="input-alert" v-if="!$v.password.required && $v.password.$error">This field must not be empty.</p>
-                        <p class="input-alert" v-if="!$v.password.isNotSameAsEmail && $v.password.$error">Password cannot be the same as the E-Mail.</p>
-                        <p class="input-alert" v-if="!$v.password.isNotSameAsUsername && $v.password.$error">Password cannot be the same as the Username.</p>
+                        <p class="input-alert" v-if="!errors.passwordError && !$v.password.minLength && $v.password.$error">Si prega di inserire almeno {{ $v.password.$params.minLength.min }} caratteri.</p>
+                        <p class="input-alert" v-if="!errors.passwordError && !$v.password.maxLength && $v.password.$error">Si prega di non inserire pi&ugrave; di {{ $v.password.$params.maxLength.max }} caratteri.</p>
+
+                        <p class="input-alert" v-if="!errors.passwordError && !$v.password.required && $v.password.$error">Questo campo non deve essere vuoto.</p>
+                        <p class="input-alert" v-if="!errors.passwordError && !$v.password.isNotSameAsEmail && $v.password.$error">La password non pu&ograve; essere uguale all'e-mail.</p>
+                        <p class="input-alert" v-if="!errors.passwordError && !$v.password.isNotSameAsUsername && $v.password.$error">La password non pu&ograve; essere uguale al nome utente.</p>
+
+                        <p class="input-alert" v-if="errors.passwordError">La password deve avere almeno una lettera, un numero e un carattere speciale: !@#$%^&amp;</p>
                 </div>
                 <!-- pre>{{$v.password}}</pre -->
                 
@@ -75,12 +85,33 @@ var VRegisterRegister = Vue.component("VRegisterRegister", {
                     <input :name="inputParams.passwordAgainParameter"
                         type="password"
                         required="required"
+                        readonly="readonly"
+                        @focus="onFieldFocus"
                         @input="$v.password2.$touch()"
-                        v-model="password2">
+                        v-model="password2"
+                        autocomplete="off">
                         <label>{{inputParams.loginPassword}}</label>
                 </div>
                 <!-- pre>{{$v.password2}}</pre -->
-                <p class="input-alert" v-if="!$v.password2.sameAs && $v.password2.$error">Please insert the same password.</p>
+                <p class="input-alert" v-if="!$v.password2.sameAs && $v.password2.$error">Si prega di inserire la stessa password.</p>
+
+                <div class="tos" @click="toggleTos($event)"> 
+                  
+                  <label class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" :name="tos" id="tos" checked="checked" v-model="tos">
+                    <span class="custom-control-label">Accetto i termini di servizio.</span>
+                  </label>
+
+                </div>
+
+                <div class="privacy" @click="togglePrivacy($event)"> 
+                  
+                  <label class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" :name="privacy" id="privacy" checked="checked" v-model="privacy">
+                    <span class="custom-control-label">Acconsento al trattamento dei dati.</span>
+                  </label>
+
+                </div>
 
                 <div class="submit">
                     <input type="submit" :disabled="$v.$invalid" class="btn btn-primary btn-block" :value="inputParams.registerSubmit" id="loginButton_submit">
@@ -97,6 +128,12 @@ var VRegisterRegister = Vue.component("VRegisterRegister", {
     inputParams: {
       required: true,
     },
+    errors: {
+      required: true,
+    },
+    inputFields: {
+      required: true,
+    },
     urls: {
       required: true,
     },
@@ -108,7 +145,10 @@ var VRegisterRegister = Vue.component("VRegisterRegister", {
       email: "",
       password: "",
       password2: "",
+      tos: false,
+      privacy: false,
       params: [],
+      fields: [],
     };
   },
 
@@ -116,40 +156,31 @@ var VRegisterRegister = Vue.component("VRegisterRegister", {
     username: {
       required,
       minLength: minLength(5),
+      maxLength: maxLength(64),
     },
     email: {
       required,
       email,
-      /*unique: value => {
-              if(value === '')
-                return true //not my job
-              return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  resolve(value !== 'test@test.it')
-                } , 1000)
-              })*/
-      /*
-                return axios.get('/users.json?newEmail=' + value )
-                  .then(res => {
-                    console.log(res)
-                    //fetch info from res.data and return boolean
-                    Object.keys(res.data).length === 0
-                  })
-                */
-      //}
     },
     password: {
       required,
       minLength: minLength(8),
+      maxLength: maxLength(64),
       isNotSameAsUsername: not(sameAs("username")),
       isNotSameAsEmail: not(sameAs("email")),
-      //passwordComplexity
     },
     password2: {
       sameAs: sameAs((vm) => {
         return vm.password;
       }),
     },
+    tos: {
+      accepted: val => val === true
+    },
+    privacy: {
+      accepted: val => val === true
+    }
+    //https://lobotuerto.com/blog/how-to-validate-a-required-boolean-with-vuelidate/
   },
 
   created() {
@@ -168,14 +199,33 @@ var VRegisterRegister = Vue.component("VRegisterRegister", {
         };
       });
     }
+    if(!isUndefined(this.inputFields)){
+      if(!isUndefined(this.inputFields.username) && this.inputFields.username != "") {
+        this.username = this.inputFields.username
+      }
+      if(!isUndefined(this.inputFields.email) && this.inputFields.email != "") {
+        this.email = this.inputFields.email
+      }
+    }
+    //Test
+    this.username = "admin@admin.it"
+    this.email = "aaa@aa.it"
+    this.password = this.password2 = "000000@a"
+    
   },
   methods: {
     onSubmit(e) {
       console.log(e);
       //submit form
     },
-    rememberMeToggle(e) {
-      console.log(e);
+    onFieldFocus(e){
+      e.target.removeAttribute('readonly')
+    },
+    toggleTos(e) {
+      this.tos = !this.tos;
+    },
+    togglePrivacy(e) {
+      this.privacy = !this.privacy;
     },
   },
 });
