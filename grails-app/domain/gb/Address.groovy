8d0b7e241eb6083974afb4bac01a6d0e93c7e62c
Luigi Zanderighi <luigi.zanderighi@gmail.com>
2021-02-09 22:54:29 +0100

    Fix #81

--- grails-app/domain/gb/Address.groovy
+++ grails-app/domain/gb/Address.groovy
@@ -15,7 +15,7 @@ class Address {
         address1 nullable:false, blank:false, size: 2..200
         address2 nullable:true, blank:true, size: 2..200
         postalCode nullable:false, blank:false, size: 5..5
-        district nullable:false, blank:false, size: 2..2
+        district nullable:false, blank:false, size: 2..200
         countryCode nullable:false, blank:false, size: 2..2
         lat nullable: false, blank: false
         lon nullable: false, blank: false
---
+++ src/database/fix-district.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `gb-dev`.`address`
+CHANGE COLUMN `district` `district` VARCHAR(200) NOT NULL ;
