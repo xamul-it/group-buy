bc6410c8f89be6802dfe9be3c411249e6e740d04
Luigi Zanderighi <luigi.zanderighi@gmail.com>
2021-02-09 22:49:40 +0100

    Fix #84 Non funziona la pagina degli ordini

--- grails-app/services/gb/OrderService.groovy
+++ grails-app/services/gb/OrderService.groovy
@@ -30,42 +30,67 @@ abstract class OrderService implements IOrderService {
     transient grails.plugin.springsecurity.SpringSecurityService  springSecurityService
 
     Order get(Map params) {
-
+        def userId = springSecurityService?.getCurrentUser()?.getId()?:0;
         def group = Group.findById(params.groupId)
-
-        def order = group ? Order.findByGroupAndId(group, params.id) : Order.findByToken(params.id)
+        def gm = GroupMember.findByUserAndGroupAndStatus(springSecurityService?.getCurrentUser(),group,MemberStatus.ACTIVE)
+        def order
+        if(gm)
+            order = group ? Order.findByGroupAndId(group, params.id) : Order.findByToken(params.id)
+        else
+            order = new Order()
+        log.debug "QUERY by user $gm and $order"
+        return order
     }
 
-    Long count (Map params){
-        def orderCount = 0
-
-        if(params.groupId) {
-            def group = Group.findById(params.groupId)
-            orderCount = group ? Order.countByGroup(group) : 0
-        } else {
-            //TODO if user logged in and no params.groupId return user's orders list
-            //placeholder code:
-            def group = Group.findById(1)
-            orderCount = group ? Order.countByGroup(group) : 0
+    /**
+     *
+     * @param params
+     *  latitude
+     *  longitude
+     * Latitude: 1 deg = 110.574 km
+     * Longitude: 1 deg = 111.320*cos(latitude) km
+     *  src
+     * @return
+     */
+    def query(Map params){
+        def userId = springSecurityService?.getCurrentUser()?.getId()?:0;
+        log.debug "QUERY by user $userId and $params"
+        def qparam= [:]
+        String q = "from Order as s where 1=1 "
+        q += "and s.user.id=  :userId "
+        qparam.userId = "$userId".toLong()
+        if (params.groupId) {
+            q += "and s.group.id=  :groupId "
+            qparam.groupId = "$params.categoryId".toLong()
         }
+        q += "order by orderDate desc"
+        log.debug("$params.categoryId : $qparam.categoryId Query $q Params: $qparam")
+        return [qparam,q]
+    }
 
-        orderCount
+
+
+    Long count (Map params){
+        def l
+        def qparam= [:]
+        String q
+        (qparam,q) = query(params)
+        def r = Order.executeQuery("select count(s) "+q , qparam);
+        return r[0]
     }
 
     List<Order> list (Map params){
-        def ordersList = []
+        def l
+        def qparam= [:]
+        String q
+        (qparam,q) = query(params)
 
-        if(params.groupId) {
-            def group = Group.findById(params.groupId)
-            ordersList = group ? Order.findAllByGroup(group, params) : []
-        } else {
-            //TODO if user logged in and no params.groupId return user's orders list
-            //placeholder code:
-            def group = Group.findById(1)
-            ordersList = group ? Order.findAllByGroup(group, params) : []
-        }
+        qparam.max=params.max
+        qparam.offset=params.offset
+        def res = Order.findAll(q , qparam)
+        log.debug("Risultato "+params.src!=null ? "++ $params.src":"--"+" $res")
 
-        ordersList
+        return res
     }
 
     Order save(Order order) {
