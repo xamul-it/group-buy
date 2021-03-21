065b7eb14e3349dc13b591657b71181f06e341c0
Luigi Zanderighi <luigi.zanderighi@gmail.com>
2021-03-21 19:28:03 +0100

    fix start test

--- scripts/start-test.sh
+++ scripts/start-test.sh
@@ -4,5 +4,5 @@ for P in $(ps aux | grep grails | grep java |grep 8080 |awk '{print $2};'); do
    kill -9 $P
 done
 cd test/
-git pull
+git pull origin dev
 grails dev run-app -port=8080 >> log.out &
